9cfe1784ff946114cda4e31bb97c60d925462797
Abhilasha Purwar <abhilasha.purwar@gmail.com>
2024-03-19 18:06:35 +0530

    auth via clerk

--- app/(auth)/(routes)/sign-in/page.tsx
+++
@@ -1,5 +0,0 @@
-const SignInPage = () => {
-  return <div>This is an unprotected Sign In Page</div>;
-};
-
-export default SignInPage;
--- app/(dashboard)/(routes)/page.tsx
+++ app/(dashboard)/(routes)/page.tsx
@@ -1,7 +1,11 @@
+import { UserButton } from "@clerk/nextjs";
+import { User } from "lucide-react";
+
 export default function Home() {
   return (
-    <div>
-      <p className="text-3xl bg-sky-700"> This is a protected page </p>
+    <div className="p-6 space-y-4">
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-4"></div>
+      <UserButton afterSignOutUrl="/" />
     </div>
   );
 }
